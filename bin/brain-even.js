e1fda90f8095f05bc81780379386263ccbdfa857
Гусев Александр <1sashagusev1@gmail.com>
2025-02-06 19:26:31 +0300

    brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainEven from '../src/game1.js';
+brainEven();
+
---
+++ src/game1.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+const isEven = (number) => number % 2 === 0;
+
+const brainEven = () => {
+    console.log('Welcome to the Brain Games!');
+
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    let correctAnswers = 0;
+
+    while (correctAnswers < 3) {
+        const randomNumber = Math.floor(Math.random() * 100);
+        console.log(`Question: ${randomNumber}`);
+
+        const answer = readlineSync.question('Your answer: ').toLowerCase();
+
+        const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+        if (answer !== correctAnswer) {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+
+        console.log('Correct!');
+        correctAnswers += 1;
+    }
+
+    console.log(`Congratulations, ${name}!`);
+};
+
+export default brainEven;
